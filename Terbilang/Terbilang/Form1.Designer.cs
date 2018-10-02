9841cf67d558b4ca178f1b2bf674866a217e9f6f
unknown <ifadwipuspitasari@gmail.com>
2018-10-02 21:12:39 +0700

    Terbilang

---
+++ Terbilang/Terbilang/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Terbilang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nilainominal = new System.Windows.Forms.TextBox();
+            this.hasilTerbilang = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nominal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Terbilang";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cek Terbilang";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnTerbilang_Click);
+            // 
+            // nilainominal
+            // 
+            this.nilainominal.Location = new System.Drawing.Point(96, 10);
+            this.nilainominal.Name = "nilainominal";
+            this.nilainominal.Size = new System.Drawing.Size(135, 20);
+            this.nilainominal.TabIndex = 3;
+            // 
+            // hasilTerbilang
+            // 
+            this.hasilTerbilang.FormattingEnabled = true;
+            this.hasilTerbilang.Location = new System.Drawing.Point(96, 37);
+            this.hasilTerbilang.Name = "hasilTerbilang";
+            this.hasilTerbilang.Size = new System.Drawing.Size(558, 43);
+            this.hasilTerbilang.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 105);
+            this.Controls.Add(this.hasilTerbilang);
+            this.Controls.Add(this.nilainominal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nilainominal;
+        private System.Windows.Forms.ListBox hasilTerbilang;
+    }
+}
+
---
+++ Terbilang/Terbilang/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Terbilang;
+
+namespace Terbilang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTerbilang_Click(object sender, EventArgs e)
+        {
+            int nominal = Convert.ToInt32(nilainominal.Text);
+            Terbilang terbilang = new Terbilang();
+
+            hasilTerbilang.Items.Add(terbilang.TerbilangIndonesia(nominal));
+        }
+    }
+}
